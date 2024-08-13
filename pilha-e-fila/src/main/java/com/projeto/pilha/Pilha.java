1230f66ef3f673885d0c57548870390800414855
murilochag <murilochagas10@outlook.com>
2024-08-13 10:58:58 -0300

    implementação de pilhas

---
+++ pilha-e-fila/src/main/java/com/projeto/pilha/Pilha.java
@@ -0,0 +1,53 @@
+package com.projeto.pilha;
+
+public class Pilha {
+
+    private No refNoEntradaPilha;
+
+    public Pilha() {
+        this.refNoEntradaPilha = null;
+    }
+
+    public No top() {
+        return refNoEntradaPilha;
+    }
+
+    public void push(No novoNo) {
+        No refAuxiliar = refNoEntradaPilha;
+        refNoEntradaPilha = novoNo;
+        refNoEntradaPilha.setRefNo(refAuxiliar);
+    }
+
+    public No pop() {
+        if(!this.isEnpty()) {
+            No noPoped = refNoEntradaPilha;
+            refNoEntradaPilha = refNoEntradaPilha.getRefNo();
+            return noPoped;
+        }
+        return null;
+    }
+
+    public boolean isEnpty() {
+        return refNoEntradaPilha == null ? true : false;
+    }
+
+    @Override
+    public String toString() {
+        String stringRetorno = "-------------\n";
+        stringRetorno += "   Pilha \n";
+        stringRetorno += "-------------\n";
+
+        No noAuxiliar = refNoEntradaPilha;
+
+        while (true) {
+            if(noAuxiliar != null) {
+                stringRetorno += "[No{dado=" + noAuxiliar.getDado() + "}]\n";
+                noAuxiliar = noAuxiliar.getRefNo();
+            } else {
+                break;
+            }
+        }
+        stringRetorno += "=============";
+        return stringRetorno;
+    }
+}
